91ccccdf5bc42917f0531b31b42f09d554c3e33f
Joel Zayas <4129984+jzayas13@users.noreply.github.com>
2024-07-01 20:41:41 -0400

    Init commit

---
+++ main.js
@@ -0,0 +1,106 @@
+const path = require("path");
+const os = require("os");
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
+const imagemin = require("imagemin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
+const slash = require("slash");
+const log = require("electron-log");
+
+// Set env
+process.env.NODE_ENV = "production";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
+let mainWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: "ImageMin",
+    width: isDev ? 800 : 500,
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev ? true : false,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile("./app/index.html");
+}
+
+app.on("ready", () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on("close", () => (mainWindow = null));
+});
+
+const menu = [
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
+];
+
+ipcMain.on("image:minimize", (e, options) => {
+  options.dest = path.join(os.homedir(), "ImageMin");
+  shrinkImage(options);
+});
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100;
+
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({
+          quality: [pngQuality, pngQuality],
+        }),
+      ],
+    });
+
+    log.info(files);
+
+    //     Changed from shell.openItem() for v9
+    shell.openPath(dest);
+
+    mainWindow.webContents.send("image:done");
+  } catch (err) {
+    log.error(err);
+  }
+}
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
+
+app.allowRendererProcessReuse = true;
---
+++ readme.md
@@ -0,0 +1,30 @@
+# ImageMin
+
+A desktop app built on Electron for minimizing images
+
+## Usage
+
+### Install Dependencies
+
+```
+npm install
+```
+
+### Run
+
+```
+npm start
+npm run dev (with Nodemon)
+```
+
+### Package
+
+```
+npm run package-mac
+npm run package-win
+npm run package-linux
+```
+
+## LICENSE
+
+MIT
